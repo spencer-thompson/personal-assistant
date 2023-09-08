d8e8e572e6aae92ba83674ec1a6b2c3b6d9899e6
Spencer <sthompson.dev@outlook.com>
2023-09-07 18:44:01 -0600

    started gpt class

--- src/gpt.py
+++ src/gpt.py
@@ -1,1 +1,22 @@
-#gpt file
+"""
+Docstring
+
+Home of the GPT Class
+
+"""
+import openai
+
+from dotenv import load_dotenv
+import os
+
+class GPT():
+
+    def __init__(
+            self,
+            model: str = "gpt-3.5-turbo"
+        ):
+        """Docstring"""
+        self.model = model
+
+        load_dotenv()
+        openai.api_key = os.getenv("OPENAI_API_KEY")
