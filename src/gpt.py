4dce35d39512b22c9ed09ca575f5d42a2e216e3f
Spencer Thompson <sthompson.dev@outlook.com>
2023-09-14 13:45:34 -0600

    or Q

--- src/gpt.py
+++ src/gpt.py
@@ -124,7 +124,7 @@ class GPT():
 if __name__ == "__main__":
     ai = GPT(model = "gpt-4")
     user_input = input(f"Chatting with {ai._model} | (q to quit):\n")
-    while user_input != "q":
+    while user_input != "q" or user_input == "Q":
         for token in ai.run(user_input):
             print(token, end='')
         user_input = input()
