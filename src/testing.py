de0227dccc2cbf24d0c92702c0b076ac59233d8d
Spencer <sthompson.dev@outlook.com>
2023-09-18 21:05:25 -0600

    attempted text to speech

--- src/testing.py
+++ src/testing.py
@@ -1,4 +1,6 @@
 #anything
+from gtts import gTTS
+import os
 
 from gpt import GPT
 
@@ -7,7 +9,12 @@ ai = GPT(system_message="You are a friendly robot, you always bring up python pr
 for i in range(3):
     user_input = input()
     for i in ai.srun(user_input):
-        print(i, end='')
+        print(i, end='[]')
+        # try: # ! This creates a ton of files on your computer
+        #     gTTS(text=i, lang='en', slow=False).save(f'{i}.mp3')
+        #     os.system(f'{i}.mp3')
+        # except AssertionError:
+        #     pass
 
 # for mes in ai.messages:
 #     print(mes)T
