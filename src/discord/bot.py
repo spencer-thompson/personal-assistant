7266883e1f6f43b5b225f75745f6c509df4ee850
BuildTools <10959652@uvu.edu>
2023-09-26 00:57:54 -0600

    Added person to bot/ai communication. Issue w path

--- src/discord/bot.py
+++ src/discord/bot.py
@@ -1,18 +1,36 @@
 import discord
 from dotenv import load_dotenv
 import os
+from gpt import GPT
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
+ai = GPT()
 
+intents = discord.Intents.default()
+# Allows bot to read messages
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
     print('Client is Online')
+
+# AI communication testing
+@client.event
+async def on_message(mess):
+    # Ignores messages by bot to avoid repeated sending of messages
+    if mess.author == client.user:
+        return
+    
+    user_input = mess.content
+    response = ai.run(user_input)
+    print(response)
+
+    # Sends message through bot to channel input was sent through
+    await mess.channel.send(response)
+
 
 
 client.run(os.getenv('bot_token'))
