4996386d56308011bd1bfad84a739a0c184677ed
BuildTools <10959652@uvu.edu>
2023-09-25 23:21:01 -0600

    Added discord folder and sample discord bot

---
+++ src/discord/bot.py
@@ -0,0 +1,18 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Client is Online')
+
+
+client.run(os.getenv('bot_token'))
